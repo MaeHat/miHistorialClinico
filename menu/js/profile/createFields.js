3a8f7ff6dcf385ec66616959d1357c075df572e1
Israel Estévez Hatchuell <hatchuell.mae@gmail.com>
2015-12-05 12:48:02 +0000

    First changes in branch
    Here is my first branch after the master.

---
+++ menu/js/profile/createFields.js
@@ -0,0 +1,29 @@
+function createFields(){
+
+    var MaxInputs = 8; //Número Maximo de Campos
+    var contenedor = $("#contenedor"); //ID del contenedor
+    var AddButton = $("#agregarCampo"); //ID del Botón Agregar
+
+    //var x = número de campos existentes en el contenedor
+    var x = $("#contenedor div").length + 1;
+    var FieldCount = x-1; //para el seguimiento de los campos
+
+    $(AddButton).click(function (e) {
+        if(x <= MaxInputs) //max input box allowed
+        {
+            FieldCount++;
+            //agregar campo
+            $(contenedor).append('<div><input type="text" name="mitexto[]" id="campo_'+ FieldCount +'" placeholder="Texto '+ FieldCount +'"/><a href="#" class="eliminar">&times;</a></div>');
+            x++; //text box increment
+        }
+        return false;
+    });
+
+    $("body").on("click",".eliminar", function(e){ //click en eliminar campo
+        if( x > 1 ) {
+            $(this).parent('div').remove(); //eliminar el campo
+            x--;
+        }
+        return false;
+    });
+};
