4c3e2e68b4e852990b5d8a95edc9ee2a0f430d90
MaeHat <hatchuell_mae@hotmail.com>
2015-12-10 19:14:06 +0000

    Última funcionalidad medicamentos
    Añadiendo editar campos

--- menu/js/controllerMedic.js
+++ menu/js/controllerMedic.js
@@ -1,15 +1,29 @@
 angular.module("myApp", []).controller("AppCtrl", function($scope){
 	$scope.nuevoMedicamento = {};
 	$scope.medicamentos = [];
+	$scope.modificando = false;
 
 	$scope.addMedic = function(){
 		if(Object.keys($scope.nuevoMedicamento).length != 0){
-			$scope.medicamentos.push($scope.nuevoMedicamento);
-			$scope.nuevoMedicamento = {};
+			if(!$scope.modificando){
+				$scope.medicamentos.push($scope.nuevoMedicamento);
+				$scope.nuevoMedicamento = {};
+			}else{
+				$scope.medicamentos[$scope.pos] = $scope.nuevoMedicamento;
+				$scope.nuevoMedicamento = {};
+				$scope.modificando = false;
+			}
 		}
 	};
 
 	$scope.remove = function(index){
 		$scope.medicamentos.splice(index, 1);
-	}
+	};
+
+	$scope.edit = function(index){
+		$scope.nuevoMedicamento = $scope.medicamentos[index];
+		$scope.modificando = true;
+		$scope.pos = index;
+
+	};
 });
